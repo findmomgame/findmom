9959d003385adf07c6cf32ca5c3e224a0ff701e0
findmomgame <0025412967@student.dthu.edu.vn>
2026-06-04 21:37:53 +0700

    Action.js

--- Action.js
+++ Action.js
@@ -277,7 +277,7 @@ function startBattleZone(zoneId) {
         const badge = document.getElementById('boss-mechanic-badge');
         if (badge) badge.style.display = 'block';
     } else {
-        gameState.monsterMaxHP = 500;
+        gameState.monsterMaxHP = 400;
         const badge = document.getElementById('boss-mechanic-badge');
         if (badge) badge.style.display = 'none';
     }
